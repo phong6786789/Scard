1b3bb7188f9f5b938d8e01525d0bc67c992f3963
UNICOM72\UNIT <phong6786789@gmail.com>
2021-05-28 09:08:59 +0700

    no message

--- app/src/main/java/com/subi/scard/view/MainActivity.kt
+++ app/src/main/java/com/subi/scard/view/MainActivity.kt
@@ -2,7 +2,9 @@ package com.subi.scard.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.subi.scard.R
+import com.subi.scard.R.id.fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
-
+    //Block back
     override fun onBackPressed() {
-        super.onBackPressed()
+//        val currentFragment = NavHostFragment.findNavController(R.id.fragment).currentDestination?.id
+//        if (currentFragment == R.id.loginFragment||currentFragment == R.id.homeFragment) {
+//            return
+//        } else {
+//            super.onBackPressed()
+//        }
+
     }
 
 }
