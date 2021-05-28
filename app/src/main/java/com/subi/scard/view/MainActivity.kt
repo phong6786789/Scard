5d760e5c123b583c85d144c0e182ba6bcd00aade
UNICOM72\UNIT <phong6786789@gmail.com>
2021-05-28 14:53:38 +0700

    onBack

--- app/src/main/java/com/subi/scard/view/MainActivity.kt
+++ app/src/main/java/com/subi/scard/view/MainActivity.kt
@@ -1,26 +1,23 @@
 package com.subi.scard.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.subi.scard.R
-import com.subi.scard.R.id.fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
     //Block back
     override fun onBackPressed() {
-//        val currentFragment = NavHostFragment.findNavController(R.id.fragment).currentDestination?.id
-//        if (currentFragment == R.id.loginFragment||currentFragment == R.id.homeFragment) {
-//            return
-//        } else {
-//            super.onBackPressed()
-//        }
-
+        val currentFragment = findNavController(R.id.fragment).currentDestination?.id
+        if (currentFragment == R.id.loginFragment||currentFragment == R.id.homeFragment) {
+            return
+        } else {
+            super.onBackPressed()
+        }
     }
 
 }
