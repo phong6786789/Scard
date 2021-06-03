3092c146951d926a29d147be12b177997210fa7e
UNICOM72\UNIT <phong6786789@gmail.com>
2021-06-03 13:43:40 +0700

    Update UX login by tritai

--- app/src/main/java/com/subi/scard/view/MainActivity.kt
+++ app/src/main/java/com/subi/scard/view/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setHasOptionsMenu(true)
     }
     //Block back if this in curren fragment
     override fun onBackPressed() {
@@ -24,26 +23,4 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        MenuInflater.inflate(R.menu.menu, menu)
-//        return super.onCreateOptionsMenu(menu)
-//    }
-
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.logout -> {
-//                logOut()
-//                return true
-//            }
-//            R.id.qr -> {
-//                Toast.makeText(activity, "QR", Toast.LENGTH_SHORT).show()
-//                return true
-//            }
-//            else -> {
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
 }
