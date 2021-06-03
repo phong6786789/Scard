7a9f40e81d7c3ca6e4e9cb97edb94a35733b8f01
UNICOM72\UNIT <phong6786789@gmail.com>
2021-06-03 17:08:21 +0700

    Update animation, add dialog

--- app/src/main/java/com/subi/scard/view/MainActivity.kt
+++ app/src/main/java/com/subi/scard/view/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.subi.scard.view
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.widget.Toast
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MotionEventCompat
 import androidx.navigation.findNavController
 import com.subi.scard.R
+import com.subi.scard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
     }
+
     //Block back if this in curren fragment
     override fun onBackPressed() {
         val currentFragment = findNavController(R.id.fragment).currentDestination?.id
@@ -23,4 +24,6 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+   
 }
