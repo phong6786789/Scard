c0080b7f8110cf2d74c3e8d5c35a38a664a2489a
Anh Duc <leanhduc22042000@gmail.com>
2021-06-15 19:58:12 +0700

    no message

---
+++ app/src/main/java/com/subi/scard/view/HealthActivity.kt
@@ -0,0 +1,12 @@
+package com.subi.scard.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.subi.scard.R
+
+class HealthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health)
+    }
+}
