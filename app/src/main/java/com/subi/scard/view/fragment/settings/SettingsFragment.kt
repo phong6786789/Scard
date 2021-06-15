cf6f406727f1bfd9a5f3d8924d7e370203564091
Phong Subi <phong6786789@gmail.com>
2021-06-15 17:51:14 +0700

    Add setting fragment

--- app/src/main/java/com/subi/scard/view/fragment/settings/SettingsFragment.kt
+++ app/src/main/java/com/subi/scard/view/fragment/settings/SettingsFragment.kt
@@ -3,11 +3,16 @@ package com.subi.scard.view.fragment.settings
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.subi.scard.BR
 import com.subi.scard.R
 import com.subi.scard.base.fragment.BaseBindingFragment
 import com.subi.scard.databinding.FragmentSettingsBinding
+import com.subi.scard.model.CustomItem
 import com.subi.scard.utils.Constants
+import com.subi.scard.view.adapter.CustomMenuAdapter
+import com.subi.scard.view.adapter.SettingAdapter
 
 @Suppress("DEPRECATION")
 class SettingsFragment : BaseBindingFragment<FragmentSettingsBinding, SettingsViewmodel>() {
@@ -19,6 +24,13 @@ class SettingsFragment : BaseBindingFragment<FragmentSettingsBinding, SettingsVi
         get() = R.layout.fragment_settings
 
     override fun initVariable(savedInstanceState: Bundle?, view: View) {
+        //Load list menu
+        viewDataBinding?.rcvSetting?.apply {
+            adapter = SettingAdapter(viewModel.list_menu, onItemClickListener())
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            hasFixedSize()
+        }
     }
 
     override fun initData(savedInstanceState: Bundle?, rootView: View) {
@@ -27,6 +39,23 @@ class SettingsFragment : BaseBindingFragment<FragmentSettingsBinding, SettingsVi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         toolbarOnlyTitle(Constants.TITLE.SETTINGS)
+    }
+
+    private fun onItemClickListener() = object : SettingAdapter.OnItemClickListener {
+        override fun onClickItem(value: CustomItem, i: Int) {
+            var idx: Int = when (i) {
+                0 -> R.id.infoFragment
+                1 -> R.id.MXHFragment
+                2 -> R.id.healthFragment
+                3 -> R.id.bankFragment
+                4 -> R.id.friendsFragment
+                5 -> R.id.moreFragment
+                else -> R.id.scanQRFragment
+            }
+            resetBottomNav()
+            findNavController().navigate(idx)
+        }
+
     }
 
 }
