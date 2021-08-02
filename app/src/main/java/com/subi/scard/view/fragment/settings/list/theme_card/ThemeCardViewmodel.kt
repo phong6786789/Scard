2f16870507d4f08f907a2d5d4942e63380508cd7
TaiLy tailt6991@gmail.com <tailt6991@gmail.com>
2021-08-03 00:57:53 +0700

    edit icon theme

--- app/src/main/java/com/subi/scard/view/fragment/settings/list/theme_card/ThemeCardViewmodel.kt
+++ app/src/main/java/com/subi/scard/view/fragment/settings/list/theme_card/ThemeCardViewmodel.kt
@@ -12,10 +12,10 @@ class ThemeCardViewmodel: BaseViewModel() {
     init {
         list_menu.addAll(
             listOf(
-                CustomItem(1, R.drawable.ic_baseline_api_24, "Giao diện thẻ card QR"),
-                CustomItem(2, R.drawable.ic_baseline_api_24, "Giao diện item MXH"),
-                CustomItem(3, R.drawable.ic_baseline_api_24, "Màu sắc hệ thống app"),
-                CustomItem(4, R.drawable.ic_baseline_api_24, "Di chuyển các nút")
+                CustomItem(1, R.drawable.ic_theme, "Giao diện thẻ card QR"),
+                CustomItem(2, R.drawable.ic_themexanh, "Giao diện item MXH"),
+                CustomItem(3, R.drawable.ic_themetim, "Màu sắc hệ thống app"),
+                CustomItem(4, R.drawable.ic_themehong, "Di chuyển các nút")
             )
         )
     }
